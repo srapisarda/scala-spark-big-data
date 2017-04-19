4d029303e672d63fa9e30b8de6f6c5b2bf6efb94
Salvatore Rapisarda <salvatorer@halfordsmedia.co.uk>
2017-04-19 12:10:49 +0100

    implemented dfSchema as StructType

--- timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
+++ timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
@@ -1,19 +1,28 @@
 package timeusage
 
-import org.apache.spark.sql.{ColumnName, DataFrame, Row}
-import org.apache.spark.sql.types.{
-  DoubleType,
-  StringType,
-  StructField,
-  StructType
-}
+import org.apache.spark.sql.types.StructType
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
-import scala.util.Random
 
 @RunWith(classOf[JUnitRunner])
 class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
+
+
+  val resource = TimeUsage.fsPath("/timeusage/atussum.csv")
+
+
+  val rdd = TimeUsage.spark.sparkContext.textFile(resource)
+
+
+  test("'dfSchema' should return a defined schema as StructType ") {
+
+    val headerColumns = rdd.first().split(",").to[List]
+    val dfSchema: StructType = TimeUsage.dfSchema(headerColumns)
+
+    assert( dfSchema != null )
+    assert( dfSchema.size ==  headerColumns.length )
+  }
 
 }
